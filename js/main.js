4a8d7750fddc51aa63ca345f8920dc7be4aeac95
youmari <youmari300@gmail.com>
2021-11-16 08:00:15 +0100

    Add javascript implementation for menu and close btn

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const menuIcon = document.querySelector(".hamb-icon");
+const menu = document.querySelector(".menu-links");
+const exitIcon = document.querySelector(".hamb-close-btn");
+
+menuIcon.addEventListener("click", () => {
+  menu.style.display = "flex";
+});
+
+exitIcon.addEventListener("click", () => {
+  menu.style.display = "none";
+});
