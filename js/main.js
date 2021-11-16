d008e31bb720511b32e7044072bd7f193b855cb8
youmari <youmari300@gmail.com>
2021-11-16 09:15:41 +0100

    Fix a bug about mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".hamb-icon");
 const menu = document.querySelector(".menu-links");
 const exitIcon = document.querySelector(".hamb-close-btn");
+const linksMenu = document.querySelectorAll('.menu-link');
 
 menuIcon.addEventListener("click", () => {
   menu.style.display = "flex";
@@ -9,3 +10,9 @@ menuIcon.addEventListener("click", () => {
 exitIcon.addEventListener("click", () => {
   menu.style.display = "none";
 });
+
+// linksMenu.forEach((link) => {
+//     link.addEventListener('click', () => {
+//         menu.style.display = "none";
+//     })
+// })
